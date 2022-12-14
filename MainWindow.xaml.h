2234898cc40a64b9ab84173f88891fa1d81b9d71
1 <1@DESKTOP-9HFBK52>
2022-12-14 19:22:02 +0900

    초기 커밋.

---
+++ MainWindow.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::App30::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+      
+    };
+}
+
+namespace winrt::App30::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
